fd8a4a53f0d9e01c928b16db8cc38a4ae0158896
Arjun Alva <69946331+arjun149@users.noreply.github.com>
2023-07-15 19:53:23 -0400

    Update odeSolver.R

--- odeSolver.R
+++ odeSolver.R
@@ -1,16 +1,23 @@
 library(deSolve)
 
 model <- function(t, x, params) {
-    # extract state variables
-    x1 <- x[1] #x
-    x2 <- x[2] #xc
-    # extract parameters
-    k, u, tx, b <- params["k", "u", "tx", "b"]
-    # model equations
-    dx1 <- (pi/12) * (x2 + b)
-    dx2 <- (pi/12) * ((u * (x2 - ((4*x^3)/2))) - (x1 * ((24/(0.99669 * tx))^2 + (k*b))))
-    # combine results into a single vector
-    dxdt <- c(dx1, dx2)
-    # return result as a list
-    list(dxdt)
+  # extract state variables
+  x1 <- x[1] #x
+  x2 <- x[2] #xc
+  # extract parameters
+  #params["k", "u", "tx", "b"]
+  
+  # model equations
+  k <- params["k"]
+  u <- params["u"]
+  tx <- params["tx"]
+  b <- params["b"]
+  
+  dx1dt <- (pi/12) * (x2 + b)
+  dx2dt <- (pi/12) * ((u * (x2 - ((4*x^3)/2))) - (x1 * ((24/(0.99669 * tx))^2 + (k*b))))
+  # combine results into a single vector
+  #dxdt <- c(dx1, dx2)
+  # return result as a list
+  return(list(c(dx1dt, dx2dt)))
 }
+
