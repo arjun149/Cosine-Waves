cf08647cf20a41e710c3f929b9c3e87e5c5e004e
Arjun Alva <69946331+arjun149@users.noreply.github.com>
2023-07-15 19:52:58 -0400

    Update cos.R

--- cos.R
+++ cos.R
@@ -7,62 +7,62 @@ library(shinythemes)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("ODE Solver"),
-    #xc
-    #u
-    #x
-    #tx
-    #B
-    #k
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("k",
-                        "K value:",
-                        min = 1,
-                        max = 10,
-                        value = 1),
-            sliderInput("xc", 
-                        "Xc value:", 
-                        min = 1, 
-                        max = 10,
-                        value = 1),
-            sliderInput("u", 
-                        "U (mew) value:",
-                        min = 1,
-                        max = 10,
-                        value = 1),
-            sliderInput("x", 
-                        "X value:",
-                        min = 1,
-                        max = 10,
-                        value = 1),
-            sliderInput("tx", 
-                        "Tx (tau-x) value:",
-                        min = 1,
-                        max = 10,
-                        value = 1),
-            sliderInput("b", 
-                        "B value:",
-                        min = 1,
-                        max = 10, 
-                        value = 1),
-                           
-             shinythemes::themeSelector(),
-        ),
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("odePlot")
-        )
+  
+  # Application title
+  titlePanel("ODE Solver"),
+  #xc
+  #u
+  #x
+  #tx
+  #B
+  #k
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("k",
+                  "K value:",
+                  min = 1,
+                  max = 10,
+                  value = 1),
+      sliderInput("xc", 
+                  "Xc value:", 
+                  min = 1, 
+                  max = 10,
+                  value = 1),
+      sliderInput("u", 
+                  "U (mew) value:",
+                  min = 1,
+                  max = 10,
+                  value = 1),
+      sliderInput("x", 
+                  "X value:",
+                  min = 1,
+                  max = 10,
+                  value = 1),
+      sliderInput("tx", 
+                  "Tx (tau-x) value:",
+                  min = 1,
+                  max = 10,
+                  value = 1),
+      sliderInput("b", 
+                  "B value:",
+                  min = 1,
+                  max = 10, 
+                  value = 1),
+      
+      shinythemes::themeSelector(),
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("odePlot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   source(file.path("odeSolver.R"), local = TRUE)$value 
-    
+  
   output$odePlot <- renderPlot({
     x0 <- c(x = input$x, xc = input$xc)
     # parameters
@@ -73,18 +73,18 @@ server <- function(input, output) {
     out <- ode(y = x0, times = times, func = model, parms = params)
     # convert result to dataframe
     out <- as.data.frame(out)
-# plot results
+    # plot results
     plot(out$time, out$x1, type = "l", xlab = "Time", ylab = "x1")
-})
-    
-#    output$cosinePlot <- renderPlot({
-#       t <- seq(0, 10, 0.1)
-       #as.numeric(input$freq)
-#       y <- sin((as.numeric(input$freq))*t)
-      
- #      qplot(t, y, geom="path", xlab="x" , ylab = "y", color = "red", fill="blue")
-        
- #   })
+  })
+  
+  #    output$cosinePlot <- renderPlot({
+  #       t <- seq(0, 10, 0.1)
+  #as.numeric(input$freq)
+  #       y <- sin((as.numeric(input$freq))*t)
+  
+  #      qplot(t, y, geom="path", xlab="x" , ylab = "y", color = "red", fill="blue")
+  
+  #   })
 }
 
 # Run the application 
