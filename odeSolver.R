e6aa663e9cfb6395800729f79795d4b0a821b3e1
Arjun Alva <69946331+arjun149@users.noreply.github.com>
2023-07-30 20:03:31 -0400

    Update odeSolver.R

--- odeSolver.R
+++ odeSolver.R
@@ -1,6 +1,8 @@
 library(deSolve)
 
 
+
+
 light <- function(t, params) {
   lux <- params["maxLux"]
   lights_on <- params["lights_on"]
@@ -22,16 +24,15 @@ model <- function(t, x, params) {
   #params["k", "u", "tx", "b"]
   
   # model equations
-  beta <- params["beta"]
-  k <- params["k"]
-  u <- params["u"]
-  tx <- params["tx"]
-  b <- params["b"]
+  beta <- as.numeric(unlist(params["beta"]))
+  k <- as.numeric(unlist(params["k"]))
+  u <- as.numeric(unlist(params["u"]))
+  tx <- as.numeric(unlist(params["tx"]))
+  b <- as.numeric(unlist(params["b"]))
   
-  dy <- as.numeric((pi/12) * (xc + b))
-  dxc <- as.numeric((pi/12) * ((u * (xc - ((4*xc**3/2)))) - (y * ((24/(0.99669 * tx))**2 + (k*b)))))
-  dn <- as.numeric(60 * (light(t, params) * (1 - n) - (beta * n)))
-  # all derivatives with respect to t
+  dy <- (pi/12) * (xc + b)
+  dxc <- (pi/12) * ((u * (xc - ((4*xc**3/2)))) - (y * ((24/(0.99669 * tx))**2 + (k*b))))
+  dn <- 60 * (light(t, params) * (1 - n) - (beta * n))
   # combine results into a single vector
   #dxdt <- c(dx1, dx2)
   # return result as a list
